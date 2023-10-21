a3ec60940b1bc1ba2cb26717b38203b4e12c97a0
Vitor Zortea <vitor.zortea@wkconsultoria.com.br>
2023-10-21 14:23:44 -0300

    build

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -24,7 +24,7 @@ export class UserService {
     signOut(this.auth)
     .then((e)=>{
       this.router.navigateByUrl('/');
-      window.location.reload();
+      //window.location.reload();
     })
   }
 }
