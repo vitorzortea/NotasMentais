a083492eeb8b4a1bf1ddc2123e70addb89ee3b12
Vitor Zortea <vitor.zortea@wkconsultoria.com.br>
2023-10-25 15:46:54 -0300

    fix mobile

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Optional } from '@angular/core';
 import { Auth, User, signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
@@ -18,13 +19,33 @@ export class UserService {
 
   logIn(email, senha) {
     signInWithEmailAndPassword(this.auth, email, senha)
-    .then((e)=>{this.router.navigateByUrl('/');});
-  }  
+    .then(
+      (e)=>{
+        this.router.navigateByUrl('/');
+    }).catch((error) => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Erro:',
+        text: this.hubMensagem[error.code],
+      })
+      console.log("ERRo: ", Object.keys(error));
+      console.log("ERRo: ", error.code);
+      console.log("ERRo: ", error.customData);
+      console.log("ERRo: ", error.name);
+    });
+  }
+
   logOut(){
     signOut(this.auth)
     .then((e)=>{
-      this.router.navigateByUrl('/');
-      //window.location.reload();
+      this.router.navigateByUrl('/auth');
     })
+  }
+
+  hubMensagem = {
+    "auth/invalid-login-credentials": 'Usuário ou Senha incorretos',
+    "auth/missing-email": 'E-mail é obrigatório',
+    "auth/invalid-email": 'E-mail invalido',
+    "auth/missing-password": 'Senha é obrigatório',
   }
 }
